8f1f321daeb96e974fc13f7aa79c84f8b6460552
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2023-12-13 23:06:13 +0100

    added dates and db functions

--- lib.js
+++ lib.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+
 const takeScreenshot = async function(page, nr) {
     const path = "screenshots/" + nr + ".png";
     console.log("Taking screenshot: " + path);
@@ -27,6 +29,78 @@ const isElementClickable = async function(page, elementHandle) {
     return true;
 }
 
+const extractDates = async function(dateRange) {
+    dateRangeSplit = dateRange.split(" ");
+    // console.log({dateRangeSplit});
+    const dates = [];
+    dateRangeSplit.forEach(element => {
+        const dateSplit = element.split(".");
+        // console.log({dateSplit});
+        if(dateSplit.length != 3) return;
+        const date = new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]);
+        // const txtDate = date.toString();
+        // console.log({txtDate});
+        dates.push(date);
+    });
+    
+    if(dates.length != 2) return [new Date(undefined)];
 
-module.exports = { takeScreenshot, isElementClickable };
+    // get dates between first and last date
+    const datesBetween = [];
+    const lastDate = dates[1];
+    let firstDate = dates[0];
+    while(firstDate <= lastDate) {
+        datesBetween.push(firstDate);
+        firstDate = new Date(firstDate.getTime() + 24 * 60 * 60 * 1000);
+        // firstDate.setDate(firstDate.getDate() + 1);
+    }
+
+    // console.log({dates});
+    const datesJoined = datesBetween.join(";");
+
+    return datesJoined;
+}
+
+const addFoodToDatabase = async function(brandName, foodInfo, newPrice, oldPrice, discountFactor, PricePerUnit, image, category, store, dates) {
+    const db = new sqlite3.Database('food.db');
+    const sql = `INSERT INTO food (brandName, foodInfo, newPrice, oldPrice, discountFactor, PricePerUnit, image, category, store, dates) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const params = [brandName, foodInfo, newPrice, oldPrice, discountFactor, PricePerUnit, image, category, store, dates];
+
+    db.run(sql, params, function(err) {
+        if (err) {
+            console.log(err.message);
+        }
+        // console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        // console.log('Close the database connection.');
+    });
+}
+
+const clearDatabaseByStore = async function(store) {
+    const db = new sqlite3.Database('food.db');
+    const sql = `DELETE FROM food WHERE store = ?`;
+    const params = [store];
+
+    db.run(sql, params, function(err) {
+        if (err) {
+            console.log(err.message);
+        }
+        // console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        // console.log('Close the database connection.');
+    });
+}
+
+
+module.exports = { takeScreenshot, isElementClickable, extractDates, addFoodToDatabase, clearDatabaseByStore };
 
