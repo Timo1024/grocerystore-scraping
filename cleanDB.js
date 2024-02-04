2bcd790dbc04917338dd79f9b1f4ce07c89c57ec
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2024-02-04 14:14:21 +0100

    added deletion of NULL rows

---
+++ cleanDB.js
@@ -0,0 +1,32 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const initializeSqliteDB = () => {
+    const db = new sqlite3.Database('food.db');
+    db.serialize(function() {
+        // db.run("CREATE TABLE IF NOT EXISTS food (brandName TEXT, foodInfo TEXT, newPrice TEXT, oldPrice TEXT, discountFactor TEXT, PricePerUnit TEXT, image TEXT, category TEXT, store TEXT, dates TEXT)");
+        db.run("CREATE TABLE IF NOT EXISTS food (foodInfo TEXT, newPrice TEXT, oldPrice TEXT, discountFactor TEXT, PricePerUnit TEXT, image TEXT, category TEXT, store TEXT, dates TEXT)");
+    });
+    db.close();
+}
+
+const clearNullRows = async function() {
+    const db = new sqlite3.Database('food.db');
+    const sql = `DELETE FROM food WHERE store IS NULL OR foodInfo IS NULL OR newPrice IS NULL`;
+
+    db.run(sql, function(err) {
+        if (err) {
+            console.log(err.message);
+        }
+        console.log(`Deleted NULL rows`);
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+}
+
+initializeSqliteDB();
+clearNullRows();
